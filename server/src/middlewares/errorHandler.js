788b2931791ad96ffc30b6873c58e5615050a9b5
horlartundhey <horlartundhey@gmail.com>
2025-12-18 11:17:34 +0100

    Improve duplicate field error messages with better context

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -14,18 +14,23 @@ const errorHandler = (err, req, res, next) => {
 
   // Mongoose duplicate key
   if (err.code === 11000) {
+    console.log('Duplicate key error:', err.keyPattern, err.keyValue);
     const field = Object.keys(err.keyPattern || {})[0];
+    const value = err.keyValue ? err.keyValue[field] : '';
     let message = 'Duplicate field value entered';
     
     // Provide more specific error messages
     if (field === 'businessSlug') {
-      message = 'A business with this name already exists. Please use a different name.';
+      message = 'A business with this name already exists. Please try a slightly different name.';
     } else if (field === 'businessEmail') {
       message = 'This email address is already registered to another business.';
     } else if (field === 'businessPhone') {
       message = 'This phone number is already registered to another business.';
+    } else if (field === 'owner') {
+      // This shouldn't happen - owner is not unique
+      message = 'Database error: Owner field incorrectly marked as duplicate. Please contact support.';
     } else if (field) {
-      message = `This ${field} is already in use. Please use a different value.`;
+      message = `The ${field.replace(/([A-Z])/g, ' $1').toLowerCase()} "${value}" is already in use. Please use a different value.`;
     }
     
     error = { message, statusCode: 400 };
